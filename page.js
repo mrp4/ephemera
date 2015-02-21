01ffbec042cb36ccfa506b77f2024bf94c6457a3
Aniqa Aslam <aa201@st-andrews.ac.uk>
2015-02-21 20:10:17 +0000

    added comments

--- page.js
+++ page.js
@@ -12,6 +12,10 @@ function toggledrawmode() {
     }
 }
 
+//event happens everytime new line created on canvas
+//ypu can set functions as callback
+//wheneer event happens, that calls the function
+//the function is anonymous
 fc.on("path:created", function(path) {
 	console.log("path:created");
 	console.log(path)
@@ -20,6 +24,8 @@ fc.on("path:created", function(path) {
 
 });
 
+//websocket; what it uses to comm in realtime
+//websocket.send to send
 var ws;
 var serverAddress = document.URL.substring(document.URL.indexOf(":") + 3, document.URL.length - 1);
 setUpHost();
@@ -47,20 +53,25 @@ function setUpHost() {
             	var path = JSON.parse(m.data.toString());
             	console.log("JSON parsed");
             	console.log(path);
+              //deafault path, don't do anything with it, replace attributes
             	var temp_path = new fabric.Path('M 0 0 L 50 0 M 0 0 L 4 -3 M 0 0 L 4 3 z', {
-				    left: 100,
+				    //default, deleted
+            left: 100,
 				    top: 100,
 				    stroke: 'red',
 				    strokeWidth: 1,
 				    fill: false
 				});
+        //replace all data attributes with the remote one
+        //k will be things like width, size; k is name instead of actual values
 				for (var k in path.path) {
 					console.log(k.toString());
 					temp_path[k] = path.path[k];
 				}
             	console.log("Received path from server:");
             	console.log(temp_path);
-            	fc.add(temp_path);
+            	//adds path to canvas
+              fc.add(temp_path);
            	} catch (ex) {
            		console.log(ex)
            	}
